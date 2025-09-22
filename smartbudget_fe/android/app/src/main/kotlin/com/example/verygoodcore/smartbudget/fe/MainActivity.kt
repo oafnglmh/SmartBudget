ccfaa8787232a7f33b54ee80eaa99c915316cf10
oafnghh <minhhoanglee11@gmail.com>
2025-09-22 15:46:26 +0700

    feat: set up project smart budget

---
+++ smartbudget_fe/android/app/src/main/kotlin/com/example/verygoodcore/smartbudget/fe/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.verygoodcore.smartbudget_fe
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ smartbudget_fe/lib/main_production.dart
@@ -0,0 +1,6 @@
+import 'package:smartbudget_fe/app/app.dart';
+import 'package:smartbudget_fe/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
